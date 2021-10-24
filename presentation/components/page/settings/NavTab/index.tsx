65a61670d97fc65a83e39db5fc7573c1d8d5a142
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-25 07:31:19 +1000

    Refactor NavTab component

--- presentation/components/page/settings/NavTab/index.tsx
+++ presentation/components/page/settings/NavTab/index.tsx
@@ -10,7 +10,7 @@ type PropsT = NavBarLink & {
 const NavTab: FC<PropsT> = (props) => {
   const { to, text, isActive } = props;
   return (
-    <Link href={to} passHref>
+    <Link href={to}>
       <StyledNavTab isActive={isActive}>
         <TabText>{text}</TabText>
       </StyledNavTab>
