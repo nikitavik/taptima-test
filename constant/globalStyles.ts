59589b47e06d2ebb8ce0dd7f773ac68e3feef7ad
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-25 09:23:03 +1000

    Add font queries

--- constant/globalStyles.ts
+++ constant/globalStyles.ts
@@ -2,14 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 import { mq } from './mediaqueries';
 
 export const GlobalStyles = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-size: 16px;
-    color: #3A3B3F;
+  html, body {
     font-family: Roboto, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 14px;
+    color: #3A3B3F;
+    overflow: hidden;
+    ${mq.xl} {
+      font-size: 18px;
+    }
     ${mq.sm} {
       font-size: 14px;
     }
--- constant/mediaqueries.ts
+++ constant/mediaqueries.ts
@@ -1,5 +1,6 @@
 export const mq = {
   xs: '@media (max-width: 768px)',
   sm: '@media (max-width: 1024px)',
-  lg: '@media (min-width: 1440px)',
+  lg: '@media (max-width: 1440px)',
+  xl: '@media (max-width: 2560px)',
 };
