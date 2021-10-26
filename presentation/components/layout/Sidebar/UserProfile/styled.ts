589d07691e422b511a4602d8abdae82ce499eda9
Nikita Kornilov <kornilov.nm@gmail.com>
2021-10-26 13:18:37 +1000

    Add missed styles to user profile to indicate a link

--- presentation/components/layout/Sidebar/UserProfile/styled.ts
+++ presentation/components/layout/Sidebar/UserProfile/styled.ts
@@ -6,19 +6,25 @@ export const StyledUserProfile = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  border-bottom: ${(props) => props.theme.borders.gray1pxBorder};
+  border-bottom: ${({ theme }) => theme.borders.gray1pxBorder};
   cursor: pointer;
+  &:hover > figure {
+    border: 2px solid ${({ theme }) => theme.colors.darkenBlue};
+  }
 `;
 export const AvatarWrapper = styled.figure`
+  box-sizing: border-box;
+  border: 2px solid transparent;
   width: 100px;
   height: 100px;
   border-radius: 50%;
   overflow: hidden;
+  transition: border 0.25s ease-in;
 `;
 export const AvatarImage = styled.img`
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.theme.colors.gray33};
+  background-color: ${({ theme }) => theme.colors.gray33};
 `;
 export const UserName = styled.h2`
   font-weight: normal;
@@ -34,5 +40,5 @@ export const UserRole = styled.h3`
   margin-top: 0.25rem;
   font-size: 0.75rem;
   line-height: 133%;
-  color: ${(props) => props.theme.colors.subFont};
+  color: ${({ theme }) => theme.colors.subFont};
 `;
